743f00f441ddf5d351dee4ae4b695404a2a99dc4
ElijahButers <Elijahbuters@gmail.com>
2015-10-18 14:23:00 +0300

    Added:
    @IBAction func plus() {
    
            currentOperator = "plus"
            userIsTypingNumber = false
    
            if numberOne == 0 && numberTwo == 0 {
                numberOne = displayValue
            } else if numberOne != 0 && numberTwo == 0 {
                numberTwo = displayValue
                displayValue = numberOne + numberTwo
                display.setText(String(displayValue))
                numberOne = displayValue
                numberTwo = 0
            }
    
        }

--- Watch - WatchCalculator WatchKit Extension/InterfaceController.swift
+++ Watch - WatchCalculator WatchKit Extension/InterfaceController.swift
@@ -114,9 +114,25 @@ class InterfaceController: WKInterfaceController {
     }
     
     @IBAction func plus() {
+        
+        currentOperator = "plus"
+        userIsTypingNumber = false
+        
+        if numberOne == 0 && numberTwo == 0 {
+            numberOne = displayValue
+        } else if numberOne != 0 && numberTwo == 0 {
+            numberTwo = displayValue
+            displayValue = numberOne + numberTwo
+            display.setText(String(displayValue))
+            numberOne = displayValue
+            numberTwo = 0
+        }
+
     }
     
     @IBAction func minus() {
+        
+        
     }
     
     @IBAction func multiply() {
